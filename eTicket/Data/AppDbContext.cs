6834e3946350f53bd586f6c80863f8ebac0ffbb7
kelkias44 <emanakelkias8@gmail.com>
2023-05-23 17:09:57 +0300

    models

--- eTicket/Data/AppDbContext.cs
+++ eTicket/Data/AppDbContext.cs
@@ -4,5 +4,8 @@ namespace eTicket.Data
 {
     public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {            
+        }
     }
 }
--- eTicket/Models/Producer.cs
+++ eTicket/Models/Producer.cs
@@ -1,6 +1,13 @@
-﻿namespace eTicket.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
 {
     public class Producer
     {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
     }
 }
